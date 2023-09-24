d75b38009aa939f86d6bcf04b92d3d66df929c1d
Nicolás <131214845+NibboNi@users.noreply.github.com>
2023-09-23 23:49:13 -0600

    Add hero section to home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,13 @@
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share <span className="orange_gradient text-center">Mindless Thoughts</span>
+      </h1>
+      <p className="dsc text-center">
+        Thoughtless Minds is the embodiment of no thoughts head empty on the internet.
+      </p>
+    </section>
   )
 }
 
