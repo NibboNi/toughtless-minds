2d0c61289d01ba86ac4eec73bcc233e0c6793292
Nicolás <131214845+NibboNi@users.noreply.github.com>
2023-09-24 01:17:38 -0600

    Add Feed component to page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,17 @@
+import Feed from "@components/Feed"
+
 const Home = () => {
   return (
-    <section className="w-full flex-center flex-col">
+    <section className="w-full flex-center flex-col gap-3">
       <h1 className="head_text text-center">
-        Discover & Share <span className="orange_gradient text-center">Mindless Thoughts</span>
+        Discover & Share
+        <span className="orange_gradient text-center"> Mindless Thoughts</span>
       </h1>
       <p className="dsc text-center">
         Thoughtless Minds is the embodiment of no thoughts head empty on the internet.
+        Share thoughs when you're at your blankest.
       </p>
+      <Feed />
     </section>
   )
 }
