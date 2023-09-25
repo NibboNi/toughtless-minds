1f66cd20e1919aeac07fd9bc3c60b00429c08397
Nicolás <131214845+NibboNi@users.noreply.github.com>
2023-09-25 13:52:32 -0600

    Update page.jsx
    Corrected some classes to fix styles

--- app/page.jsx
+++ app/page.jsx
@@ -2,12 +2,13 @@ import Feed from "@components/Feed"
 
 const Home = () => {
   return (
-    <section className="w-full flex-center flex-col gap-3">
+    <section className="w-full flex-center flex-col">
       <h1 className="head_text text-center">
-        Discover & Share
+        Discover & Share 
+        <br className="max-md:hidden" />
         <span className="orange_gradient text-center"> Mindless Thoughts</span>
       </h1>
-      <p className="dsc text-center">
+      <p className="desc text-center">
         Thoughtless Minds is the embodiment of no thoughts head empty on the internet.
         Share thoughs when you're at your blankest.
       </p>
